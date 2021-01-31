522d3f67f637f1de54e4a4c32493b4b36284b5dd
Chris Dusyk <chris.dusyk@gmail.com>
2021-01-31 02:53:42 -0600

    Create animation stuffs

--- Assets/Scripts/DialogueWindow.cs
+++ Assets/Scripts/DialogueWindow.cs
@@ -6,57 +6,10 @@ using TMPro;
 
 public class DialogueWindow : MonoBehaviour
 {
-	private const string ALPHA_CODE = "<color=#00000000>";
-
 	public TMP_Text Text;
-	private string[] _allDialogue;
 
-	CanvasGroup Group;
-
-	void Start()
+	public void DisplayDialogueText(string text)
 	{
-		Group = GetComponent<CanvasGroup>();
-		Group.alpha = 0;
-	}
-
-	public void Show(string[] text)
-	{
-		Group.alpha = 1;
-		_allDialogue = text;
-		StartCoroutine(DisplayText());
-	}
-
-	public void Close()
-	{
-		StopAllCoroutines();
-		Group.alpha = 0;
-	}
-
-	private IEnumerator DisplayText()
-	{
-		foreach (var dialogue in _allDialogue)
-		{
-			Text.text = "";
-
-			var originalText = dialogue;
-			var displayedText = string.Empty;
-			var alphaIndex = 0;
-
-			foreach (var c in originalText.ToCharArray())
-			{
-				++alphaIndex;
-				Text.text = originalText;
-				displayedText = Text.text.Insert(alphaIndex, ALPHA_CODE);
-				Text.text = displayedText;
-
-				yield return new WaitForSecondsRealtime(0.03f);
-			}
-
-			yield return new WaitForSecondsRealtime(1f);
-		}
-
-		Group.alpha = 0;
-
-		yield return null;
+		Text.SetText(text);
 	}
 }

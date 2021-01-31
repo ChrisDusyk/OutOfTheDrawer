9b9381f3df49c70148ee6b4a3748764fe17e2f91
Chris Dusyk <chris.dusyk@gmail.com>
2021-01-31 03:03:48 -0600

    Add support for triggering audio clips in dialogue sequence

--- Assets/Scripts/DialogueWindow.cs
+++ Assets/Scripts/DialogueWindow.cs
@@ -7,9 +7,15 @@ using TMPro;
 public class DialogueWindow : MonoBehaviour
 {
 	public TMP_Text Text;
+	public AudioSource AudioSource;
 
 	public void DisplayDialogueText(string text)
 	{
 		Text.SetText(text);
+	}
+
+	public void TriggerAudioClip(AudioClip audio)
+	{
+		AudioSource.PlayOneShot(audio);
 	}
 }
